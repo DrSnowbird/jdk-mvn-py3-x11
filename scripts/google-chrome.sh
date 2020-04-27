6488c454066383672bdab5a6f1970b013c6c4a40
openkbs <rsheu@openkbs.org>
2020-04-27 14:24:48 -0400

    remove obmenu install due to google-chrome error and it is not used

--- scripts/google-chrome.sh
+++ scripts/google-chrome.sh
@@ -5,15 +5,19 @@ set -e
 echo "Install Google Chromium Browser"
 cd /tmp
 
-sudo apt update -y 
+sudo apt-get update -y 
+sudo apt-get upgrade -y 
+
+# -- patch (work-around) fix to prevent 
+#echo "Set disable_coredump false" | sudo tee -a /etc/sudo.conf
 
 # (Ubuntu 16.04 only)
 # sudo apt-get install -y libindicator3-7 indicator-application libnss3-nssdb libnss3 libnspr4 libappindicator3-1 fonts-liberation xdg-utils
-sudo apt-get install -y libindicator3-7 indicator-application libnss3-dev libnss3-tools libnss3 libnspr4 libappindicator3-1 fonts-liberation xdg-utils
+sudo apt-get install -y libindicator3-7 indicator-application libnss3-dev libnss3-tools libnss3 libnspr4 libappindicator3-1 fonts-liberation xdg-utils libgbm1 libu2f-udev
 
 wget -q -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 which google-chrome
 
@@ -21,7 +25,7 @@ rm google-chrome-stable_current_amd64.deb
 
 echo "CHROMIUM_FLAGS='--no-sandbox --no-gpu --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
 
-sudo apt-get install -y xvfb openbox obmenu tint2 xfce4-panel xfce4-notifyd xfce4-whiskermenu-plugin compton feh conky-all
+sudo apt-get install -y xvfb openbox tint2 xfce4-panel xfce4-notifyd xfce4-whiskermenu-plugin compton feh conky-all
 
 sudo /etc/init.d/dbus restart
 sudo service dbus start
@@ -38,5 +42,5 @@ cat >./google-chrome <<-EOF
 EOF
 sudo cp ./google-chrome /opt/google/chrome/google-chrome
 
-sudo apt autoremove -y
+sudo apt-get autoremove -y
 
